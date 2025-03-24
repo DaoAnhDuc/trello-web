8c6f0f56c39ec3c057c472fcc9b27808e172683c
dinhdt@aladintech.co <dinhdt@aladintech.co>
2025-03-24 16:43:41 +0700

    add css baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Button from '@mui/material/Button'
-import './App.css'
 import AccessibleIcon from '@mui/icons-material/Accessible'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 function App() {
   return (
     <>
+      <div>Hello</div>
       <Button variant="contained">Hello world</Button>
       <AccessibleIcon/>
       <HomeIcon />
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material'
+
 import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
