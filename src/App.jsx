414032a92e8ab3821aa6fe19bad38947358c90cc
Anh Duc <daoanhduc2604@gmail.com>
2025-04-06 22:23:32 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { Box, Container } from "@mui/material";
-import React from "react";
-import ModeSelect from "./components/ModeSelect";
-import Board from "./pages/Boards/_id";
+import Board from "~/pages/Boards/_id";
 
 const App = ({}) => {
   return (
