257b4ffea7cc534b3680c42071221daf0d05d90b
Anh Duc <daoanhduc2604@gmail.com>
2025-04-06 21:03:02 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import Button from '@mui/material/Button'
-import AccessibleIcon from '@mui/icons-material/Accessible'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-function App() {
-  return (
-    <>
-      <div>Hello</div>
-      <Button variant="contained">Hello world</Button>
-      <AccessibleIcon/>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
-  )
-}
+import { Box, Container } from "@mui/material";
+import React from "react";
+import ModeSelect from "./components/ModeSelect";
 
-export default App
+const App = ({}) => {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh", backgroundColor: "primary.main" }}>
+      <Box
+        sx={{
+          backgroundColor: "primary.light",
+          height: (theme) => theme.trello.appBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.dark",
+          height: (theme) => theme.trello.boardBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Boad bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          height: (theme) => `calc(100% - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Box Content
+      </Box>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/ModeSelect.jsx
@@ -0,0 +1,43 @@
+import { Box, MenuItem, FormControl, InputLabel, Select } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import { useColorScheme } from "@mui/material/styles";
+import React from "react";
+
+const ModeSelect = () => {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select labelId="label-select-dark-light-mode" id="select-dark-light-mode" value={mode} label="Mode" onChange={handleChange}>
+        <MenuItem value="light">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+export default ModeSelect;
