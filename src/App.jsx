31baf89d9bf5f4944edf0dbbe204035622bfc1a3
Anh Duc <daoanhduc2604@gmail.com>
2025-04-06 21:21:08 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
 import { Box, Container } from "@mui/material";
 import React from "react";
 import ModeSelect from "./components/ModeSelect";
+import Board from "./pages/Boards/_id";
 
 const App = ({}) => {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: "100vh", backgroundColor: "primary.main" }}>
-      <Box
-        sx={{
-          backgroundColor: "primary.light",
-          height: (theme) => theme.trello.appBarHeight,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <ModeSelect />
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "primary.dark",
-          height: (theme) => theme.trello.boardBarHeight,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        Boad bar
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          height: (theme) => `calc(100% - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        Box Content
-      </Box>
-    </Container>
+    <>
+      <Board />
+    </>
   );
 };
 
